f1fa00dfb8c0f999ab6f13074f04ff9e45529675
sheikh-muzammil2026 <discordsstudent@gmail.com>
2026-07-14 20:05:32 +0600

    added mongodb uri and password in .env

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,3 +30,9 @@ export default function RootLayout({
     </html>
   );
 }
+
+// MONGODB_URI=mongodb+srv://gearshare:eU4IsxJhtgL6cmBS@cluster0.xxxx.mongodb.net/?retryWrites=true&w=majority
+// MONGODB_URI=mongodb://gearshare:eU4IsxJhtgL6cmBS@ac-famfzlt-shard-00-00.w9cbrwo.mongodb.net:27017,ac-famfzlt-shard-00-01.w9cbrwo.mongodb.net:27017,ac-famfzlt-shard-00-02.w9cbrwo.mongodb.net:27017/gearshare?ssl=true&replicaSet=atlas-131uq2-shard-0&authSource=admin&appName=Cluster0
+
+// JWT_SECRET=your_super_secret_jwt_key_here
+// NEXTAUTH_URL=http://localhost:3000
