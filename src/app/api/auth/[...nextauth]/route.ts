30eebdb029086824fdf7e3fd074b7aa4f099f2aa
Sheikh Muzammil <discordsstudent@gmail.com>
2026-07-15 19:10:40 +0600

    Create nextAuth on  route.ts

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,18 @@
+// app/api/auth/[...nextauth]/route.ts
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/login", // আপনার কাস্টম লগইন পেজ
+  },
+});
+
+export { handler as GET, handler as POST };
