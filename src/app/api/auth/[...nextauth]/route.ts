91c26f6a2c1bcb41eddd1f9cdc1121f955a7fdcb
Sheikh Muzammil <discordsstudent@gmail.com>
2026-07-16 06:55:30 +0600

    Added nextauthin  route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,10 @@
-// app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
 
 const handler = NextAuth({
+  adapter: MongoDBAdapter(clientPromise), // 🔐 এটি গুগল দিয়ে লগইন করা মাত্রই ডাটাবেজে ইউজার সেভ করবে
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
@@ -10,8 +12,11 @@ const handler = NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt", // সেশন ম্যানেজমেন্টের জন্য টোকেন ট্র্যাকিং সুবিধা রাখবে
+  },
   pages: {
-    signIn: "/login", // আপনার কাস্টম লগইন পেজ
+    signIn: "/login",
   },
 });
 
